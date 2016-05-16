810bf8651a4a5d47e07e6e277f7e68eede0018c4
Raúl G. González <raulggonzalez@justojs.org>
2016-05-16 10:27:49 +0200

    Fix #1

--- dist/es5/nodejs/justo-plugin-unzip/lib/op.js
+++ dist/es5/nodejs/justo-plugin-unzip/lib/op.js
@@ -4,10 +4,7 @@
 
 
 
-
-
-
-op;var _path = require("path");var _path2 = _interopRequireDefault(_path);var _yauzl = require("yauzl");var _yauzl2 = _interopRequireDefault(_yauzl);var _fs = require("fs");var _fs2 = _interopRequireDefault(_fs);var _justoFs = require("justo-fs");function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function op(params, done) {
+op;var _justoUnzip = require("justo-unzip");var _justoUnzip2 = _interopRequireDefault(_justoUnzip);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function op(params, done) {
   var opts;
 
 
@@ -17,34 +14,4 @@ op;var _path = require("path");var _path2 = _interopRequireDefault(_path);var _y
   if (!opts.dst) throw new Error("dst expected.");
 
 
-  unzip(opts.src, opts.dst, opts, done);}
-
-
-function unzip(src, dst, opts, done) {
-  _yauzl2.default.open(src, { lazyEntries: true }, function (err, zip) {
-
-    if (err) return done(err);
-
-
-    zip.readEntry();
-    zip.on("close", function () {
-      done();});
-
-
-    zip.on("entry", function (entry) {
-      if (entry.fileName.endsWith("/")) {
-        var dir = new _justoFs.Dir(dst, entry.fileName);
-
-        if (!dir.exists()) dir.create();
-        zip.readEntry();} else 
-      {(function () {
-          var file = new _justoFs.File(dst, entry.fileName);
-          var dir = file.parent;
-
-          zip.openReadStream(entry, function (err, readStream) {
-            if (err) return done(err);
-
-            if (!dir.exists()) dir.create();
-            readStream.pipe(_fs2.default.createWriteStream(file.path));
-            readStream.on("end", function () {
-              zip.readEntry();});});})();}});});}
+  (0, _justoUnzip2.default)(opts.src, opts.dst, done);}
